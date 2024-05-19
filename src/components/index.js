67c17faa5694928e5da81a8cefd0db93f0f32e9e
kunalsharma3012 <kunalsharma93103@gmail.com>
2024-05-19 22:45:55 +0530

    currencyConverter

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// we are making this because , if we make more than two files then we import those files here then export to the
+// main file
+
+import InputBox from './InputBox'
+
+export {InputBox}
